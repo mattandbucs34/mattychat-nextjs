1d426a8eceb18b3cc1f32a8f92b4a4b5717bc672
Matt Johnson <mattandbucs@gmail.com>
2025-12-14 17:22:08 -0600

    change env vars to not be NEXT_PUBLIC_

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,13 +22,13 @@ import { grey } from '@mui/material/colors';
 
 // Firebase config
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    apiKey: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_APP_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 };
 
 const firebase = initializeApp(firebaseConfig);
